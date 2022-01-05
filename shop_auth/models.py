8a10fce3b2724361dd786eef1dea2075a0240ff8
phikko <skeezze@gmail.com>
2022-01-05 22:50:43 +0100

    changed user login field to email

--- shop_auth/models.py
+++ shop_auth/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
